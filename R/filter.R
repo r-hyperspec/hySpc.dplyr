702fb213f5bc5a8756b9d6c2c2e898fecdb3555b
Claudia Beleites <Claudia.Beleites@chemometrix.gmbh>
2020-03-19 12:02:54 +0100

    move splice to own file

--- R/filter.R
+++ R/filter.R
@@ -39,6 +39,8 @@
 filter.hyperSpec <- function(.data, ..., .preserve = FALSE) {
 
   .data@data <- filter (.data@data, ..., .preserve = .preserve)
+  .data <- .fix_spc_colnames(.data)
+
   chk.hy(.data)
 
   .data
@@ -88,47 +90,3 @@ filter.hyperSpec <- function(.data, ..., .preserve = FALSE) {
   })
 }
 
-
-#' @inheritParams dplyr::slice
-#' @rdname filter.hyperSpec
-#' @importFrom dplyr slice
-#' @export
-#'
-#' @examples
-#' chondro %>% slice (1:3)
-#' chondro %>% slice (800 : n())
-#' chondro %>% slice (-10 : -n())
-
-slice.hyperSpec <- function(.data, ..., .preserve = FALSE) {
-
-  .data@data <- slice (.data@data, ..., .preserve = .preserve)
-  chk.hy(.data)
-
-  .data
-}
-
-.test(slice.hyperSpec) <- function (){
-  context("slice")
-
-  test_that ("simple slicing", {
-    expect_equal(slice (flu, 1:3), flu [1:3])
-    expect_equal(slice (flu, 0), flu [0])
-
-    # slice drops row names, so only equivalent, not equal:
-    expect_equivalent (slice (flu, 3 : n()), flu [3 : nrow (flu)])
-
-    expect_equal(slice (flu, -3 : -n()), flu [-3 : -nrow (flu)])
-  })
-
-  test_that ("multiple index parameters", {
-    expect_equivalent (slice (chondro, 1:3, n() - (0:3)),
-                       chondro [c (1:3, nrow (chondro) - (0:3))]
-    )
-  })
-
-  test_that("grouping and slice", {
-    skip ("grouping not yet implemented")
-  })
-}
-
-#slice_.hyperSpec <- slice.hyperSpec
