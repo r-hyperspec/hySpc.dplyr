16fe4e25a50d72591874a6999098c95b3789d0db
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-06-12 01:15:40 -0500

    Add mutate.R

--- R/mutate.R
+++ R/mutate.R
@@ -1,9 +1,7 @@
-#' mutate for hyperSpec objects
+#' mutate and transmute for hyperSpec objects
 #'
-#' dplyr::mutate() adds new variables and preserves all the existing variables.
-#' dplyr::transmute() adds and only keeps new variables.
+#' @description dplyr::mutate() adds new variables and preserves all the existing variables.
 #' Special column `$spc` contains the spectra matrix.
-#' If `$spc` is not being transmuted, the result is a data.frame instead of a hyperSpec object.
 #'
 #' @inheritParams dplyr::mutate
 #' @inheritParams dplyr::transmute
@@ -19,46 +17,24 @@
 #' @importFrom dplyr transmute
 #'
 #' @examples
-
-#' # Mutate -----------------------------------------------
-#' chondro %>%
-#'   mutate (y, x, filename, spc) %>%
-#'   head # => results in a hyperSpec object
-#' chondro %>%
-#'   mutate (y, x)
-#'   head # => results in a hyperSpec object
-#' chondro %>%
-#'   mutate (y, x, filename, spc = spc*2)
-#'   head # => results in a hyperSpec object
-#' flu %>%
-#'   mutate (spc, filename, c)
-#'   head # => results in a hyperSpec object
-#' flu %>%
-#'   mutate (filename, c)
-#'   head # => results in a hyperSpec object
-#' flu %>%
-#'   mutate (c=c*2, c=c*0)
-#'   # => results in a hyperSpec object
 #'
-#' # Transmute -----------------------------------------------
-#' chondro %>%
-#'   transmute (y, x, filename, spc) %>%
+#' fauxCell %>%
+#'   mutate (x, y)
 #'   head # => results in a hyperSpec object
-#' chondro %>%
-#'   transmute (y, x)
-#'   head # => results in a data frame
-#' chondro %>%
-#'   transmute (y, x, filename, spc = spc*2)
+#' fauxCell %>%
+#'    mutate (x = y, y = x) # => results in a hyperSpec object
+#' fauxCell %>%
+#'    mutate (x2 = y, y2 = x) # => results in a hyperSpec object
+#' fauxCell %>%
+#'    mutate (y, x, spc2 = spc*2) # => results in a hyperSpec object
+#' fauxCell %>%
+#'    mutate (spc2 = spc*2) %>%
+#'    mutate (spc2) %>%
+#'    mutate (spc2*2) # => results in a hyperSpec object
+#' fauxCell %>%
+#'   mutate (y, x, spc) %>%
 #'   head # => results in a hyperSpec object
-#' flu %>%
-#'   transmute (spc, filename, c)
-#'   head # => results in a hyperSpec object
-#' flu %>%
-#'   transmute (filename, c)
-#'   head # => results in a data frame
-#' flu %>%
-#'   transmute (c=c*2, c=c*0)
-#'   # => results in a data frame
+#'
 #' @export
 mutate.hyperSpec <- function(.data, ...) {
 
@@ -66,13 +42,69 @@ mutate.hyperSpec <- function(.data, ...) {
     chk.hy(.data)
 
     # Collect mutate arguments
-    mutate_args <- get_args(.data, ...)
+    mutate_args <- pre_mutation(.data, ...)
 
     # Pass mutate arguments to dplyr::mutate
     .data@data <- eval(parse(text = paste("mutate(mutate_args$tmp_data,", mutate_args$args, ")")))
     .data
 }
 
+.test(mutate.hyperSpec) <- function() {
+  context("mutate.hyperSpec")
+
+  # UT1
+  test_that("non hyperSpec objects are rejected", {
+    df <- data.frame(a = NA, b = NA)
+    expect_error(mutate.hyperSpec(df))
+  })
+
+  # UT2
+  test_that("arguments are correctly passed onto mutate/transmute", {
+    df <- fauxCell
+    df <- dplyr::mutate(df@data, x, x = 0, x = 1)
+    expect_identical(mutate.hyperSpec(fauxCell, x, x = 0, x = 1)@data, df)
+  })
+
+  # UT3
+  test_that("$spc cannot be mutated", {
+    expect_error(mutate.hyperSpec(fauxCell, x, spc*2))
+    expect_error(mutate.hyperSpec(fauxCell, x, spc = c))
+    expect_error(mutate.hyperSpec(fauxCell, x, spc = spc))
+    expect_error(mutate.hyperSpec(fauxCell, x, spc = spc*2))
+  })
+
+  # UT4
+  test_that("$spc can be used for mutation", {
+    hy_tmp <- fauxCell
+    hy_tmp@data$spc2 <- hy_tmp@data$spc*2
+    expect_equivalent(mutate.hyperSpec(fauxCell, spc2 = spc*2), hy_tmp)
+  })
+}
+
+#' @description dplyr::transmute() adds and only keeps new variables.
+#' @rdname mutate.hyperSpec
+#' @aliases mutate.hyperSpec
+#' Special column `$spc` contains the spectra matrix.
+#' If `$spc` is not being transmuted, the result is a data.frame instead of a hyperSpec object.
+#'
+#' @examples
+#'
+#' fauxCell %>%
+#'   transmute (x, y)
+#'   head # => results in a data frame
+#' fauxCell %>%
+#'    transmute (x = y, y = x) # => results in a data frame
+#' fauxCell %>%
+#'    transmute (x2 = y, y2 = x) # => results in a data frame
+#' fauxCell %>%
+#'    transmute (y, x, spc2 = spc*2) # => results in a hyperSpec object
+#' fauxCell %>%
+#'    transmute (spc2 = spc*2) %>%
+#'    transmute (spc2) %>%
+#'    transmute (spc2*2) # => results in a hyperSpec object
+#' fauxCell %>%
+#'   transmute (x, y, spc) %>%
+#'   head # => results in a hyperSpec object
 #' @export
 transmute.hyperSpec <- function(.data, ...) {
 
@@ -80,7 +112,7 @@ transmute.hyperSpec <- function(.data, ...) {
     chk.hy(.data)
 
     # Collect transmute arguments
-    transmute_args <- get_args(.data, ...)
+    transmute_args <- pre_mutation(.data, ...)
 
     # Pass transmute arguments to dplyr::transmute
     res <- eval(parse(text = paste("transmute(transmute_args$tmp_data,", transmute_args$args, ")")))
@@ -88,9 +120,42 @@ transmute.hyperSpec <- function(.data, ...) {
     # Update labels
     setLabels.select(.data, res)
 }
-# Helper(s) -----------------------------------------------
 
-get_args <- function(.data, ...) {
+.test(transmute.hyperSpec) <- function() {
+  context("transmute.hyperSpec")
+
+  # UT1
+  test_that("non hyperSpec objects are rejected", {
+    df <- data.frame(a = NA, b = NA)
+    expect_error(transmute.hyperSpec(df))
+  })
+
+  # UT2
+  test_that("arguments are correctly passed onto transmute", {
+    df <- fauxCell
+    df <- dplyr::transmute(df@data, x, x = 0, x = 1)
+    fauxdf <- transmute.hyperSpec(fauxCell, x, x = 0, x = 1)
+    expect_equivalent(fauxdf, df)
+  })
+
+  # UT3
+  test_that("$spc cannot be mutated", {
+    expect_error(mutate.hyperSpec(fauxCell, x, spc*2))
+    expect_error(transmute.hyperSpec(fauxCell, x, spc = c))
+    expect_error(transmute.hyperSpec(fauxCell, x, spc = spc))
+    expect_error(transmute.hyperSpec(fauxCell, x, spc = spc*2))
+  })
+
+  # UT4
+  test_that("$spc can be used for mutation", {
+    hy_tmp <- fauxCell
+    hy_tmp@data$spc2 <- hy_tmp@data$spc*2
+    expect_equivalent(mutate.hyperSpec(fauxCell, spc2 = spc*2), hy_tmp)
+  })
+}
+
+# Helper(s) -----------------------------------------------
+pre_mutation <- function(.data, ...) {
 
   # Collect function arguments
   args <- enquos(...)
@@ -101,73 +166,91 @@ get_args <- function(.data, ...) {
     return(NULL)
   }
 
-  # Prepare a copy of the original hyperSpec object
+  # Make a copy of the original hyperSpec object
   tmp_hy <- .data
-  cols2get <- vector() # creates a list to save the column names to
+  cols2get <- vector()
 
   # Prepare function arguments for mutate/transmute
   # assumption: the variable name and expr
   # share the same index (i.e., args_name[i] is the expr for the variable names(args[i]))
   for (i in seq_along(args)) {
     expr <- quo_name(quo_get_expr(args[[i]]))
-    col_tmp <- charmatch(colnames(.data), deparse(substitute(expr)))
-    if (col_tmp[!is.na(col_tmp)] > 0) {
-      var_expr <- eval(parse(text = paste("tmp_hy@data$", expr, sep="")))
-    }
-    print(args_names[i])
-    print(expr)
-    # Process arguments with no names (assignments)
-    if ("" %in% args_names[i]) {
-      cols2get <- c(cols2get, expr)
-      # Process matrix argument assignments
-      # Manipulate `matrix column before passing it on to mutate/transmute
-    } else if (args_names[i] %in% colnames(.data) && is.matrix(var_expr)) {
+    col_name <- trimws(gsub("[[:punct:]].*","", expr), "right") # "base" expr ~should~ be in colnames(.data)
+    # print(col_name)
+    # print(args_names[i])
 
-      # Handle misuse of spc column
-      if (grepl("spc", deparse(substitute(expr))) && !"spc" %in% args_names[i]) {
-
-        # Throw an error
-        stop("$spc can only be mutated from itself")
-      }
-      tmp_hy@data[[args_names[i]]] <- .data@data[[args_names[i]]] # ensures operation on original column
-      tmp_hy@data[[args_names[i]]] <- var_expr
-      cols2get <- c(cols2get, args_names[i])
-      # Process non matrix argument assignments
+    # Capture expression value
+    if (!grepl("\\D", expr)) {
+      expr_val <- eval(parse(text = expr))
     } else {
-      print("Here")
-      assign <- paste(args_names[i],"=", expr, sep="")
-      cols2get <- c(cols2get, assign)
+      expr_val <- eval(parse(text = paste0("tmp_hy@data$", expr)))
+    }
+
+    # Argument has no name (only an expression)
+    if ("" %in% args_names[i]) {
+
+      # Expression is a column with row matrices
+      if (is.matrix(expr_val)) {
+
+        # Mutation is being performed on `spc``
+        if ("spc" %in% col_name && grepl('[^[:alnum:]]', expr)) {
+
+          # Throw error
+          stop("$spc column cannot be mutated")
+
+          # Collect `spc` column
+        } else if ("spc" %in% expr) {
+
+          # Store expr in column
+          cols2get <- c(cols2get, 'spc')
+
+          # Update temporary hyperSpec object before passing it to mutate/transmute
+        } else {
+
+          # Update tmp_hy@data
+          tmp_hy@data[[col_name]] <- expr_val
+
+          # Store expr in column (# just store `mat` not `mat`+anything_else)
+          cols2get <- c(cols2get, col_name)
+        }
+
+        # Column already exist in the hyperSpec object
+      } else {
+
+        # Store "base" expr in column
+        cols2get <- c(cols2get, expr)
+      }
+
+      # Expression's name (args_name[i]) is not empty
+    } else {
+
+      # Mutation is being performed on `spc`
+      if ("spc" %in% args_names[i]) {
+
+        # Throw error
+        stop("$spc column cannot be a named argument")
+
+        # Expression is a column with row matrices
+      } else if (is.matrix(expr_val)) {
+
+        # Update tmp_hy@data
+        tmp_hy@data[[args_names[i]]] <- expr_val
+
+        # Store "base" expr in column
+        cols2get <- c(cols2get, args_names[i])
+
+        # "vanilla" assignment
+      } else {
+
+        # Create an assignment using paste
+        assign <- paste0(args_names[i], "=", expr)
+
+        # Store expr in column
+        cols2get <- c(cols2get, assign)
+      }
     }
   }
-
-  # Hand off columns (i.e., prepared arguments) to mutate()/transmute()
-  cols2get <- unique(cols2get) # transmute/mutate already take care of this...
-  ls_hy <- list(tmp_data = tmp_hy@data, args = paste(cols2get, collapse=", "))
-}
-
-.test(mutate.hyperSpec) <- function() {
-  context("mutate.hyperSpec")
-
-  # UT1
-  test_that("non hyperSpec objects are rejected", {
-    df <- data.frame(a = NA, b = NA)
-    expect_error(mutate.hyperSpec(df))
-    expect_error(transmute.hyperSpec(df))
-  })
-
-  # UT2
-  test_that("arguments are correctly passed onto mutate/transmute", {
-    df <- flu
-    df <- dplyr::transmute(df@data, c, c = 0, c = 1)
-    expect_equivalent(transmute.hyperSpec(flu, c, c = 0, c = 1), df)
-  })
-
-  # UT3
-  test_that("$spc can only be mutated from itself", {
-    expect_error(transmute.hyperSpec(flu, c, spc = c))
-    expect_error(transmute.hyperSpec(flu, c, spc = filename*0, spc = spc))
-    df <- flu
-    df@data$spc <- df@data$spc*2
-    expect_equal(transmute.hyperSpec(flu, spc = spc*2), select(df, spc))
-  })
+    # Hand off columns (i.e., prepared arguments) to mutate()/transmute()
+    cols2get <- unique(cols2get) # transmute/mutate might already take care of this...
+    return(list(tmp_data = tmp_hy@data, args = paste(cols2get, collapse=", ")))
 }
