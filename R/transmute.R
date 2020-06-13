60a5525dda7af6a78a7d4e88b8149bdd3b8e0b53
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-06-13 05:45:42 -0500

    Update transmute.R with dplyr 1.0.0
    since dplyr 1.0.0 got published it natively works with columns containing matrices

--- R/transmute.R
+++ R/transmute.R
@@ -1,38 +1,40 @@
-#' @description dplyr::transmute() adds and only keeps new variables.
-#' @rdname mutate.hyperSpec
-#' @aliases mutate.hyperSpec
+#' transmute for hyperSpec object
+#'
+#' `transmute` adds new variables and drops all pre-existing variables.
 #' Special column `$spc` contains the spectra matrix.
 #' If `$spc` is not being transmuted, the result is a data.frame instead of a hyperSpec object.
 #'
-#' @examples
+#' @inheritParams dplyr::transmute
 #'
-#' chondro %>%
-#'   transmute (x, y) %>%
+#' @return hyperSpec object.
+#' @include unittest.R
+#' @include setLabels.R
+#' @seealso [dplyr::transmute()]
+#' @importFrom hyperSpec chk.hy
+#' @importFrom hyperSpec labels labels<-
+#' @importFrom rlang enquos
+#' @importFrom rlang quo_name
+#' @importFrom rlang quo_get_expr
+#' @importFrom dplyr transmute
+#' @export
+#'
+#' @examples
+#' laser %>%
+#'   transmute (t, filename)
 #'   head # => results in a data frame
-#' chondro %>%
-#'    transmute (x = y, y = x) # => results in a data frame
-#' chondro %>%
-#'    transmute (x2 = y, y2 = x) # => results in a data frame
-#' chondro %>%
-#'    transmute (y, x, spc2 = spc*2) # => results in a hyperSpec object
-#' chondro %>%
+#' laser %>%
+#'    transmute (-spc) # => results in a hyperSpec object
+#' laser %>%
 #'    transmute (spc2 = spc*2) %>%
 #'    transmute (spc2) %>%
 #'    transmute (spc2*2) # => results in a hyperSpec object
-#' chondro %>%
-#'   transmute (x, y, spc) %>%
-#'   head # => results in a hyperSpec object
-#' @export
 transmute.hyperSpec <- function(.data, ...) {
 
   # Check if user passed in a hyperSpec object
   chk.hy(.data)
 
-  # Collect transmute arguments
-  transmute_args <- pre_mutation(.data, ...)
-
   # Pass transmute arguments to dplyr::transmute
-  res <- eval(parse(text = paste("transmute(transmute_args$tmp_data,", transmute_args$args, ")")))
+  res <- transmute(.data@data, ...)
 
   # Update labels
   setLabels.select(.data, res)
@@ -41,31 +43,14 @@ transmute.hyperSpec <- function(.data, ...) {
 .test(transmute.hyperSpec) <- function() {
   context("transmute.hyperSpec")
 
-  # UT1
   test_that("non hyperSpec objects are rejected", {
     df <- data.frame(a = NA, b = NA)
     expect_error(transmute.hyperSpec(df))
   })
 
-  # UT2
-  test_that("arguments are correctly passed onto mutate/transmute", {
-    df <- flu
-    df <- dplyr::transmute(df@data, c, c = 0, c = 1)
-    expect_equivalent(transmute.hyperSpec(flu, c, c = 0, c = 1), df)
-  })
-
-  # UT3
-  test_that("$spc cannot be mutated", {
-    expect_error(mutate.hyperSpec(chondro, x, spc*2))
-    expect_error(transmute.hyperSpec(chondro, x, spc = c))
-    expect_error(transmute.hyperSpec(chondro, x, spc = spc))
-    expect_error(transmute.hyperSpec(chondro, x, spc = spc*2))
-  })
-
-  # UT4
   test_that("$spc can be used for mutation", {
-    hy_tmp <- chondro
+    hy_tmp <- .testdata
     hy_tmp@data$spc2 <- hy_tmp@data$spc*2
-    expect_equivalent(mutate.hyperSpec(chondro, spc2 = spc*2), hy_tmp)
+    expect_identical(mutate.hyperSpec(.testdata, spc2 = spc*2), hy_tmp)
   })
 }
