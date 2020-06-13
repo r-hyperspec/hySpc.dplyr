a5c4586fad97050e8ae16a87d35e418ee8742f05
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-06-13 07:12:51 -0500

    Update hy_update_labels.R
    Fix description params

--- R/hy_update_labels.R
+++ R/hy_update_labels.R
@@ -1,18 +1,20 @@
 #' Ensure that hyperSpec and non hyperSpec objects have the correct labels
 #'
-#' @param .data FIXME: it should be explained what `.data` is.
-#' @param res FIXME: it should be explained what `res` is.
+#' @param .data hyperSpec object
+#' @param res resulting data frame
 #' @return Object with the correct labels
 #' @md
 #' @export
 hy_update_labels <- function(.data, res){
+
   # Update labels
   labels <- labels(.data)[c(".wavelength", colnames(res))]
-  if(is.null(res$spc)){
+  if (is.null(res$spc)) {
+
     # use attribute to have correct labels when piping into `as.hyperSpec`
     attr(res, "labels") <- labels
     res
-  }else{
+  } else {
     .data@data <- res
     labels(.data) <- labels
     .data
