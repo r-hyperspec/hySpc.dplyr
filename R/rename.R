dad2cc17aa596330b9d92c0b2e8b5e3b10fac33a
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-06-17 08:36:55 -0500

    Update rename.R
    Add `setLabels_rename`, clean up test, and enforce tidyverse styling

--- R/rename.R
+++ R/rename.R
@@ -10,6 +10,7 @@
 #'
 #'
 #' @inheritParams dplyr::rename
+#' @include setLabels.R
 #' @seealso [dplyr::rename()]
 #' @return hyperSpec object. If `$spc` is renamed, the result is an error.
 #' @include unittest.R
@@ -20,29 +21,26 @@
 #'
 #' @examples
 #'
-#' chondro %>%
-#'   rename (region = clusters) %>%
+#' laser %>%
+#'   rename (filename2 = filename) %>%
 #'   head # results in a hyperSpec object
-rename.hyperSpec <- function(.data, ...){
+rename.hyperSpec <- function(.data, ...) {
+
   # Check if user passed in a hyperSpec object
   chk.hy(.data)
+
   # Use dplyr::rename() to rename hyperSpec object data slot
   res <- rename(.data@data, ...)
+
   # Check if $spc was renamed
-  if(!'spc' %in% colnames(res)){
+  if (!'spc' %in% colnames(res)) {
+
     # Throw an error
     stop("$spc cannot be renamed")
-  }else{
-    # Get new and current column names
-    labels.to.update <- setdiff(colnames(res), colnames(.data))
-    labels.to.remove <- setdiff(colnames(.data), colnames(res))
-    # Update the data slot with newly renamed data frame
-    .data@data <- res
-    # Update labels of hyperSpec object
-    new.labels <- lapply(labels(.data, labels.to.update), as.expression)
-    labels(.data)[c(labels.to.remove)] <- NULL
-    labels(.data)[c(labels.to.update)] <- new.labels
-    .data
+  } else {
+
+    # Set labels
+    setLabels_rename(.data, res)
   }
 }
 
@@ -50,18 +48,15 @@ rename.hyperSpec <- function(.data, ...){
 .test(rename.hyperSpec) <- function(){
   context("rename.hyperSpec")
 
-  # UT1
   test_that("non hyperSpec objects are rejected", {
     df <- data.frame(a = NA, b = NA)
     expect_error(rename.hyperSpec(df))
   })
 
-  # UT2
   test_that("renaming $spc throws an error", {
     expect_error(rename.hyperSpec(chondro, spc_newname = spc))
   })
 
-  # UT3
   test_that("labels attribute when returning hyperSpec object", {
     tmp <- rename.hyperSpec(chondro, region = clusters)
     tmp <- rename.hyperSpec(tmp, clusters = region)
